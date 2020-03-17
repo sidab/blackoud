3191d46baba8362968bb94bcc8306fbdec1a8817
sidab <39728359+sidab@users.noreply.github.com>
2020-03-17 10:13:38 +0300

    fix

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@ var app = new Framework7({
     root: '#app',
     name: 'Black Oud',
     theme: 'ios',
-    version: 2.9,
+    version: 3.0,
     routes: routes,
     init: false,
     backend: 'http://new.blackoud.ru/',
